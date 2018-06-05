6eade9fabdabe03e7174185ee6a774928b61696d
levankhang <levankhang197@gmail.com>
2018-06-06 02:10:34 +0700

    first commit

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,23 @@
+<section id="content">
+	<div class="row wraper">
+		<div class="container">
+			<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 list-menu pull-left">
+                    <?php $this->load->view('frontend/modules/category'); ?> 
+                </div>
+            </div>
+			<div class = "col-xs-12 col-sm-12 col-md-9 col-lg-9 product-content" id="list-content">
+				<div class="product-wrap" style="">
+                    <div class="wrap-2" style="
+    margin-bottom: 20px;
+">
+                        <h1 style="color: white">
+                            Demo website bán hàng
+                        </h1>
+
+                    </div>
+                </div>
+			</div>
+		</div>
+	</div>
+</section>
